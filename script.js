4c5deefa9188a3caf7cc71dca7575bab5de12c53
ZoricmaTs <tszzzch@gmail.com>
2020-03-03 18:54:39 +0700

    new fails added. Fails changed js, html, css

---
+++ script.js
@@ -0,0 +1,11 @@
+var filterName = 'filterName';
+
+var control = document.querySelector('selector');
+if (control) {
+  control.classList.add('active');
+}
+
+var photo = document.querySelector('.photo');
+if (photo) {
+  photo.classList.add('class');
+}
