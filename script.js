17e12842ed141a5eaff4219a4fac750ce641636f
ZoricmaTs <tszzzch@gmail.com>
2020-03-03 19:24:47 +0700

    added function toggleFilter in script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var filterName = 'walden';
-
-var control = document.querySelector('button.' + filterName);
-if (control) {
-  control.classList.add('active');
-}
-
-var photo = document.querySelector('.photo');
-if (photo) {
-  photo.classList.add(filterName);
+function toggleFilter(filterName) {
+    var control = document.querySelector('button.' + filterName);
+    if (control) {
+      control.classList.add('active');
+    }
+    
+    var photo = document.querySelector('.photo');
+    if (photo) {
+      photo.classList.add(filterName);
+    }
 }
